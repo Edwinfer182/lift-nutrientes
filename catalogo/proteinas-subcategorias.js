f62b7f3939489a79799f57393604e0fc872f9902
Edwinfer182 <edwinfdo182@gmail.com>
2026-09-07 21:08:36 -0500

    Mover Itholate e ISO-XP a proteínas limpias

--- catalogo/proteinas-subcategorias.js
+++ catalogo/proteinas-subcategorias.js
@@ -4,7 +4,7 @@
     { name: 'Proteína vegana', keywords: ['vegan','plant protein','plant-based','pea','arveja','rice protein','proteina de arroz','soya','soy','vegetal'] },
     { name: 'Proteína de carne', keywords: ['beef','carnivor','carne'] },
     { name: 'Proteína de otras fuentes', keywords: ['casein','caseina','micellar','egg','huevo','albumin','albumina'] },
-    { name: 'Proteína aislada / limpia', keywords: ['iso triple zero','triple zero','iso100','iso 100','isolate','isolated','isolation','isopure','iso whey','whey iso','isolate 100','zero carb','low carb'] },
+    { name: 'Proteína aislada / limpia', keywords: ['itholate','iso-xp','iso xp','iso triple zero','triple zero','iso100','iso 100','isolate','isolated','isolation','aislado 100% suero','aislado','isopure','iso whey','whey iso','isolate 100','zero carb','low carb'] },
     { name: 'Proteína Whey', keywords: ['whey','gold standard','syntha','combat','nitro tech','nitrotech'] }
   ];
 
