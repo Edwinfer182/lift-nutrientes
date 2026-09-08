d8a6f96cb0f41c21bf2aa80ae5718d41051778a4
Edwinfer182 <edwinfdo182@gmail.com>
2026-09-08 08:37:54 -0500

    Ampliar asociaciones de imágenes de creatinas

--- catalogo/creatinas-imagenes-drive.js
+++ catalogo/creatinas-imagenes-drive.js
@@ -5,17 +5,17 @@
   const images = [
     { aliases:['cell tech 3 lb','cell tech 3lbs','cell tech 3 libras'], id:'1Ltfp2lD4vRKKwD_R_ERmfqUm1JOljMpb' },
     { aliases:['cell tech 6 lb','cell tech 6lbs','cell tech 6 libras'], id:'12zfD4aDhk5nH78rMWrNo9-RFBeu1phUb' },
-    { aliases:['cell tech creactor','creactor 120','creactor'], id:'1WfJJSzeLMAAc-GZOqDH_HpvSe8h_abAQ' },
-    { aliases:['crea stack','creastack'], id:'1vwCWMGdlcATLB3ZeYG_lgtuyxdeKBtYQ' },
-    { aliases:['creatina chews','creatine chews','chews 90'], id:'1U73goE-Ej9Sou1Pfqc5iSBLuER1NGqF2' },
-    { aliases:['creatina dymatize','creatine dymatize','dymatize 300'], id:'1unY0XVVrUOgxhlEOJPduO-W0yBggMNc9' },
-    { aliases:['creatina iron','creatine iron','iron 100 serv'], id:'1WgTsfF74d80FErFRBZ_qsN_pDnB08hD_' },
-    { aliases:['creatina on 240','creatine on 240','optimum nutrition 240','creatina optimum 240'], id:'15Se4597oGm8jWGpKMDkYOgx45h4P68ff' },
-    { aliases:['creatina platinum 90','creatine platinum 90','platinum 90 serv'], id:'1jEUIurIkIP0eNAxAxd28hWtLspMezTFn' },
-    { aliases:['creatina simply 300','creatine simply 300','simply 300'], id:'1SOvlTG1-OG2DBtersnefTbk-NHAwuww8' },
-    { aliases:['creatine basic 300','creatina basic 300','basic 300'], id:'1DbWHPWzi2DgEbvkkPl9Rhjn1pAJ-1vwR' },
-    { aliases:['creatine simply 1 kg','creatina simply 1 kg','simply 1 kg','simply 1000'], id:'1LHbMA2OJ8SO7kQWNAeecfJyNyEV8z7i-' },
-    { aliases:['legacy 50','legacy creatine','creatina legacy'], id:'1CtXgrkeOHQCrXNjnbOQJLAcOip_WvGTP' }
+    { aliases:['cell tech creactor 120 serv','cell tech creactor','creactor 120','creactor'], id:'1WfJJSzeLMAAc-GZOqDH_HpvSe8h_abAQ' },
+    { aliases:['crea stack 60 serv','crea stack','creastack'], id:'1vwCWMGdlcATLB3ZeYG_lgtuyxdeKBtYQ' },
+    { aliases:['creatina chews 90 tabletas','creatina chews','creatine chews','chews 90'], id:'1U73goE-Ej9Sou1Pfqc5iSBLuER1NGqF2' },
+    { aliases:['creatina dymatize 300 gr 60 serv','creatina dymatize 300 gr','creatina dymatize','creatine dymatize','dymatize 300'], id:'1unY0XVVrUOgxhlEOJPduO-W0yBggMNc9' },
+    { aliases:['creatina iron 100 serv 500 gr','creatina iron 100 serv','creatina iron','creatine iron','iron 100 serv'], id:'1WgTsfF74d80FErFRBZ_qsN_pDnB08hD_' },
+    { aliases:['creatina 1200 gr 240 serv micronizada','1200 gr 240 serv micronizada','creatina on 240','creatine on 240','optimum nutrition 240','creatina optimum 240'], id:'15Se4597oGm8jWGpKMDkYOgx45h4P68ff' },
+    { aliases:['creatina platinum 450 gr 90 serv micronizada','creatina platinum 90 serv 450 gr micronizada','creatina platinum 90','creatine platinum 90','platinum 90 serv','platinum creatina 60 serv 300 gr creapure'], id:'1jEUIurIkIP0eNAxAxd28hWtLspMezTFn' },
+    { aliases:['simply creatina 300gr 60 serv','simply creatina 300 gr 60 serv','creatina simply 300','creatine simply 300','simply 300'], id:'1SOvlTG1-OG2DBtersnefTbk-NHAwuww8' },
+    { aliases:['basic creatina 300 gr 60 serv','basic creatina 300gr 60 serv','creatine basic 300','creatina basic 300','basic 300'], id:'1DbWHPWzi2DgEbvkkPl9Rhjn1pAJ-1vwR' },
+    { aliases:['simply creatina 3 1 kg 200 serv','simply creatina 1 kg 200 serv','creatine simply 1 kg','creatina simply 1 kg','simply 1 kg','simply 1000'], id:'1LHbMA2OJ8SO7kQWNAeecfJyNyEV8z7i-' },
+    { aliases:['legacy 50 serv','legacy 50','legacy creatine','creatina legacy'], id:'1CtXgrkeOHQCrXNjnbOQJLAcOip_WvGTP' }
   ];
 
   const norm = s => String(s || '').toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g,'').replace(/[^a-z0-9]+/g,' ').replace(/\s+/g,' ').trim();
